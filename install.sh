4cd1d4337c9cbec58fff4cb506e22b6084381582
Chao Liu <yijinliu@gmail.com>
2021-08-07 00:00:59 +0000

    Update install.sh.

--- install.sh
+++ install.sh
@@ -2,5 +2,5 @@
 
 set -e 
 
-sudo apt install -y --no-install-recommends influxdb python3-pip
+sudo apt install -y --no-install-recommends build-essential influxdb python3-dev python3-pip
 sudo pip3 install --upgrade Adafruit_DHT RPi.GPIO influxdb
